c0cd742875e898686ee8d0bd75bb978d0fb40751
Yuliana <yuliana.vladimirovna.02@bk.ru>
2023-04-02 16:15:26 +0300

    page, comment

--- blog/models.py
+++ blog/models.py
@@ -3,17 +3,28 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200) # заголовок
-    author = models.ForeignKey(  # автор (отношение многие-к-одному)
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
         "auth.User",
         on_delete=models.CASCADE,
     )
     body = models.TextField()
 
-
     def __str__(self):
         return self.title
 
-
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    comment = models.TextField(max_length=400)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.comment[:60]
